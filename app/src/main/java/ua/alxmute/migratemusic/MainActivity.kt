60a1a78eadefcf31112e3f02d2233250726535c6
Oleksii Mutianov <38989947+oleksii-mutianov@users.noreply.github.com>
2020-05-24 16:59:12 +0300

    Implement choosing directory (#1)

--- app/src/main/java/ua/alxmute/migratemusic/MainActivity.kt
+++ app/src/main/java/ua/alxmute/migratemusic/MainActivity.kt
@@ -1,12 +1,77 @@
 package ua.alxmute.migratemusic
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import net.rdrei.android.dirchooser.DirectoryChooserConfig
+import net.rdrei.android.dirchooser.DirectoryChooserFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), DirectoryChooserFragment.OnFragmentInteractionListener {
+
+    companion object {
+        private val PERMISSIONS = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+        private const val READ_STORAGE_PERMISSION = 1
+    }
+
+    private val chooserFragment: DirectoryChooserFragment = DirectoryChooserFragment
+            .newInstance(DirectoryChooserConfig.builder()
+                    .newDirectoryName("DialogSample")
+                    .build()
+            )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (!hasPermissions(this, PERMISSIONS)) {
+            ActivityCompat.requestPermissions(this, PERMISSIONS, READ_STORAGE_PERMISSION)
+        }
+
+        btnChoose.setOnClickListener {
+            chooserFragment.show(fragmentManager, null)
+        }
+    }
+
+    override fun onSelectDirectory(path: String) {
+        textDirectory.text = path
+        chooserFragment.dismiss()
+    }
+
+    override fun onCancelChooser() {
+        chooserFragment.dismiss()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            READ_STORAGE_PERMISSION -> {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(
+                            this, "Permission granted! Please click on pick a file once again.",
+                            Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Toast.makeText(
+                            this, "Permission denied to read your External storage :(",
+                            Toast.LENGTH_SHORT
+                    ).show()
+                    ActivityCompat.requestPermissions(this, PERMISSIONS, READ_STORAGE_PERMISSION)
+                }
+                return
+            }
+        }
+    }
+
+    private fun hasPermissions(context: Context, permissions: Array<String>): Boolean {
+        for (permission in permissions) {
+            if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false
+            }
+        }
+        return true
     }
 }
