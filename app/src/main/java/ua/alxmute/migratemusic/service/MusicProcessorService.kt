11e6e0a07db513b8a419417619cfc7052b290956
Oleksii Mutianov <38989947+oleksii-mutianov@users.noreply.github.com>
2020-11-04 17:46:57 +0200

    Make music be processed asynchronously (#23)

--- app/src/main/java/ua/alxmute/migratemusic/service/MusicProcessorService.kt
+++ app/src/main/java/ua/alxmute/migratemusic/service/MusicProcessorService.kt
@@ -1,6 +1,10 @@
 package ua.alxmute.migratemusic.service
 
 import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import ua.alxmute.migratemusic.data.ContextHolder
 import ua.alxmute.migratemusic.data.LocalTrackDto
 import ua.alxmute.migratemusic.service.factory.AddTrackChainFactory
@@ -8,6 +12,8 @@ import ua.alxmute.migratemusic.service.factory.MusicServiceStrategyFactory
 import ua.alxmute.migratemusic.service.listener.TrackProcessingListener
 
 object MusicProcessorService {
+
+    private val coroutineScope = CoroutineScope(Job() + Dispatchers.Default)
 
     fun addTracks(
         tracksToProcess: List<LocalTrackDto>,
@@ -18,18 +24,20 @@ object MusicProcessorService {
         val musicServiceStrategy = MusicServiceStrategyFactory[ContextHolder.musicServiceName]
 
         tracksToProcess.forEach { track ->
-            val result: Boolean = AddTrackChainFactory.handle(track, musicServiceStrategy)
+            coroutineScope.launch {
+                val result: Boolean = AddTrackChainFactory.handle(track, musicServiceStrategy)
 
-            // TODO: discover what to do with not migrated tracks
-            if (result) {
-                Log.d("success", "${track.author} ${track.title}")
-            } else {
-                Log.d("failure", "${track.fileName} (${track.author} ${track.title})")
+                // TODO: discover what to do with not migrated tracks
+                if (result) {
+                    Log.d("success", "${track.author} ${track.title}")
+                } else {
+                    Log.d("failure", "${track.fileName} (${track.author} ${track.title})")
+                }
+
+                processedTracks.add(track)
+
+                listener.onTrackProcessed(processedTracks.size, tracksToProcess.size)
             }
-
-            processedTracks.add(track)
-
-            listener.onTrackProcessed(processedTracks.size, tracksToProcess.size)
         }
     }
 
